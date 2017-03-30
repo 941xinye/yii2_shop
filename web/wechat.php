380f6b6b0afe3445b8907f72846c9c2625edd10d
3030944332 <3030944332@qq.com>
2017-03-31 01:56:44 +0800

    .

--- web/wechat.php
+++ web/wechat.php
@@ -9,13 +9,11 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/wechat.php');
 
-$app = new Application($config);
+$app = new \EasyWeChat\Foundation\Application($config);
 $server = $app->server;
 $user = $app->user;
-$server->setMessageHandler(function($message) use ($user) {
-    $fromUser = $user->get($message->FromUserName);
-
-    return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
+$server->setMessageHandler(function($message){
+    return "您好！欢迎关注 overtrue!";
 });
 
 $server->serve()->send();
