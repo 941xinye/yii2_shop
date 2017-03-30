31cf5686e790a28d1151b7e8e92c1526732686a2
3030944332 <3030944332@qq.com>
2017-03-31 00:59:46 +0800

    .

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -23,36 +23,10 @@ class WechatController extends Controller
         $user = $app->user;
         $server->setMessageHandler(function($message) use ($user) {
             $fromUser = $user->get($message->FromUserName);
-            switch ($message->MsgType) {
-                case 'event':
-                    $msg = '收到事件消息';
-                    break;
-                case 'text':
-                    $msg = '收到文字消息';
-                    break;
-                case 'image':
-                    $msg = '收到图片消息';
-                    break;
-                case 'voice':
-                    $msg = '收到语音消息';
-                    break;
-                case 'video':
-                    $msg = '收到视频消息';
-                    break;
-                case 'location':
-                    $msg = '收到坐标消息';
-                    break;
-                case 'link':
-                    $msg = '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    $msg = "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
-                    break;
-            }
-            return $msg;
+
+            return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
         });
 
-        return $server->serve()->send();
+        $server->serve()->send();
     }
 }
